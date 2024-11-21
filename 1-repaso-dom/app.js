70d0c5ac05031a008eec4a14ccf564c8b8a19106
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-11-20 18:57:40 -0800

    feat: borrar un to-do

--- 1-repaso-dom/app.js
+++ 1-repaso-dom/app.js
@@ -3,7 +3,7 @@ const todoInput = document.querySelector('#todo-input');
 const todosContainer = document.querySelector('#todos-container');
 const form = document.querySelector('#create-todo-form');
 
-const todos = ['sacar al perro', 'hacer el super'];
+let todos = ['sacar al perro', 'hacer el super'];
 
 // function clickHandler(event) {
 function submitHandler(event) {
@@ -22,12 +22,45 @@ function submitHandler(event) {
   todoInput.value = '';
 }
 
+function deleteHandler(event) {
+  const itemToDelete = event.target.parentNode;
+  console.log(itemToDelete);
+
+  const textToDelete = itemToDelete.querySelector('span').textContent;
+  console.log(textToDelete)
+
+
+  const newTodos = [];
+
+  // debugger
+
+  for(let i = 0; i < todos.length; i++) {
+    if(textToDelete !== todos[i]) {
+      newTodos.push(todos[i]);
+    }
+  }
+
+  todos = newTodos;
+  renderTodos()
+}
+
 function renderTodos() {
   todosContainer.innerHTML = '';
   
   for( let i = 0; i < todos.length; i++) {
     const todoItem = document.createElement('div');
-    todoItem.textContent = todos[i];
+
+    const todoContent = document.createElement('span');
+    todoContent.textContent = todos[i];
+
+    const button = document.createElement('button');
+    button.textContent = 'delete';
+
+    button.addEventListener('click', deleteHandler)
+
+    todoItem.appendChild(todoContent);
+    todoItem.appendChild(button);
+
     todosContainer.appendChild(todoItem);
   }
 }
