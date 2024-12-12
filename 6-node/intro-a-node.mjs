7de4f47c2282bdda752f600b06e7df2e53875d14
Yaxche Manrique <yaxchemanrique@gmail.com>
2024-12-11 19:29:53 -0800

    feat: introduccion a node.js

---
+++ 6-node/intro-a-node.mjs
@@ -0,0 +1,19 @@
+import { suma, resta } from './math.mjs';
+
+console.log('Hola desde intro-a-Node.js')
+
+console.log(suma(1, 3))
+console.log(resta(1, 3))
+
+/* console.log(suma(5, 4))
+console.log(suma(2, 5))
+console.log(suma(7, 7)) */
+
+/* const obj = {
+  firstName: 'Yaxche',
+  insta: '@yaxchemanrique',
+}
+
+// const firstName = obj.firstName
+const { firstName } = obj
+console.log(firstName) */
---
+++ 6-node/math.mjs
@@ -0,0 +1,16 @@
+console.log('Este es el archivo Math.mjs')
+
+export function suma(a, b) {
+  return a + b;
+}
+
+export function resta(a, b) {
+  return a - b;
+}
+
+/* const exports = {
+  suma,
+  resta
+}
+
+export default exports; */
